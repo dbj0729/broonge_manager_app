5a2f7a7160646accab1935c2d77b21729f84553d
MinHwan <gozld5153@naver.com>
2022-12-06 16:16:46 +0900

    Add eslintrc, prettierrc, patch-package, telnetClient

--- src/components/IotController.tsx
+++ src/components/IotController.tsx
@@ -1,33 +1,62 @@
-import {View, Text, Pressable} from 'react-native';
-import React, {Dispatch, SetStateAction} from 'react';
-import tw from '../lib/tailwind';
-import {iotStatusStyle} from '../lib/iotStatus';
+import { View, Text, Pressable, ActivityIndicator } from 'react-native'
+import React, { Dispatch, SetStateAction, useState } from 'react'
+import tw from '../lib/tailwind'
+import { iotStatusStyle } from '../lib/iotStatus'
+import Telnet from 'react-native-telnet-client'
 
 const IotController = ({
   marker,
   setTargetMarker,
 }: {
-  marker: any;
-  setTargetMarker: Dispatch<SetStateAction<any>>;
+  marker: any
+  setTargetMarker: Dispatch<SetStateAction<any>>
 }) => {
-  if (!marker) return null;
+  const [unLockLoading, setUnlockLoading] = useState(false)
+  const [pageLoading, setPageLoading] = useState(false)
+
+  const updateBikeStatus = async (order: string) => {
+    if (order === 'unlock') setUnlockLoading(true)
+    if (order === 'page') setPageLoading(true)
+
+    const connection = new Telnet()
+
+    const params = {
+      // host: '192.168.0.57',
+      host: 'broonge.co.kr',
+      port: 9090,
+      negotiationMandatory: false,
+      timeout: 10000,
+    }
+
+    try {
+      await connection.connect(params)
+    } catch (error) {
+      console.log('timeout?')
+      setUnlockLoading(false)
+      setPageLoading(false)
+      return
+    }
+
+    const res = await connection.send(`a001,1241212319,${order}`)
+    console.log(res)
+
+    await connection.end()
+    if (order === 'unlock') setUnlockLoading(false)
+    if (order === 'page') setPageLoading(false)
+  }
+
+  if (!marker) return null
+
   return (
-    <View
-      style={tw` absolute bottom-0 bg-white w-full rounded-tl-lg rounded-tr-lg`}>
+    <View style={tw` absolute bottom-0 bg-white w-full rounded-tl-lg rounded-tr-lg`}>
       <View
         style={tw`px-5 py-2 flex-1 flex flex-row justify-between items-center border-b border-b-gray-300`}>
         <View style={tw``}>
           <Text>{marker.bike_id}(세종)</Text>
         </View>
         <View style={tw`flex-1 flex flex-row items-center justify-end`}>
-          <View
-            style={tw`w-4 h-4 rounded-full ${
-              iotStatusStyle[marker.status].style
-            }`}
-          />
-          <Text style={tw`ml-1 text-sm`}>
-            {iotStatusStyle[marker.status].status}
-          </Text>
+          <View style={tw`w-4 h-4 rounded-full ${iotStatusStyle[marker.status].style}`} />
+          <Text style={tw`ml-1 text-sm`}>{iotStatusStyle[marker.status].status}</Text>
           <View style={tw`w-8 py-1 flex flex-row items-end justify-center `}>
             {marker.signal <= 1 ? (
               <Text style={tw`text-xs`}>N/A</Text>
@@ -54,10 +83,7 @@ const IotController = ({
             {/* <View style={tw`w-1 h-5 bg-black mr-[2px]`} /> */}
           </View>
           <View style={tw`ml-3`}>
-            <Text
-              style={tw`${
-                marker.battery <= 20 ? 'text-red-400' : ''
-              } text-xs text-center`}>
+            <Text style={tw`${marker.battery <= 20 ? 'text-red-400' : ''} text-xs text-center`}>
               {marker.battery}%
             </Text>
             <View
@@ -65,9 +91,9 @@ const IotController = ({
                 marker.battery <= 20 ? 'border-red-400' : ''
               } w-8 border p-[1px] rounded-md`}>
               <View
-                style={tw`${
-                  marker.battery <= 20 ? 'bg-red-400' : 'bg-black'
-                } h-1 w-[${marker.battery}%]  rounded-md`}
+                style={tw`${marker.battery <= 20 ? 'bg-red-400' : 'bg-black'} h-1 w-[${
+                  marker.battery
+                }%]  rounded-md`}
               />
             </View>
           </View>
@@ -75,7 +101,7 @@ const IotController = ({
           <Pressable
             hitSlop={4}
             onPress={() => {
-              setTargetMarker(undefined);
+              setTargetMarker(undefined)
             }}
             style={tw`ml-4`}>
             <Text style={tw`text-base font-bold`}>X</Text>
@@ -85,19 +111,21 @@ const IotController = ({
       <View style={tw`px-5 py-2`}>
         <View style={tw`flex-1 flex-row items-center`}>
           <Pressable
+            onPress={() => updateBikeStatus('unlock')}
             style={tw`flex-1 py-3  mx-1 items-center justify-center border rounded-md`}>
-            <Text>해제</Text>
+            {!unLockLoading && <Text>해제</Text>}
+            {unLockLoading && <ActivityIndicator animating={unLockLoading} color="#222" />}
           </Pressable>
           <Pressable
+            onPress={() => updateBikeStatus('page')}
             style={tw`flex-1 py-3  mx-1 items-center justify-center border rounded-md`}>
-            <Text>찾기</Text>
+            {!pageLoading && <Text>찾기</Text>}
+            {pageLoading && <ActivityIndicator animating={pageLoading} color="#222" />}
           </Pressable>
-          <Pressable
-            style={tw`flex-1 py-3  mx-1 items-center justify-center border rounded-md`}>
+          <Pressable style={tw`flex-1 py-3  mx-1 items-center justify-center border rounded-md`}>
             <Text>정보</Text>
           </Pressable>
-          <Pressable
-            style={tw`flex-1 py-3  mx-1 items-center justify-center border rounded-md`}>
+          <Pressable style={tw`flex-1 py-3  mx-1 items-center justify-center border rounded-md`}>
             <Text>신고</Text>
           </Pressable>
         </View>
@@ -117,7 +145,7 @@ const IotController = ({
         </View>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default IotController;
+export default IotController
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,25 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import {Image, Pressable, Text, View} from 'react-native';
-import NaverMapView, {Marker} from 'react-native-nmap';
-import IotController from '../components/IotController';
-import tw from '../lib/tailwind';
-import dummyMarker from '../../data/dummyMarker.json';
-import {iotStatusStyle} from '../lib/iotStatus';
-import {AppStackProps} from '../types/navigation';
-import {TextInput} from 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { Image, Pressable, View } from 'react-native'
+import NaverMapView, { Marker } from 'react-native-nmap'
+import IotController from '../components/IotController'
+import tw from '../lib/tailwind'
+import dummyMarker from '../../data/dummyMarker.json'
+import { iotStatusStyle } from '../lib/iotStatus'
+import { AppStackProps } from '../types/navigation'
+import { TextInput } from 'react-native-gesture-handler'
 
-const MapPage = ({navigation}: AppStackProps<'Map'>) => {
-  const P0 = {latitude: 33.45061368551521, longitude: 126.56895152804822};
-  const [markers, setMarkers] = useState<any[]>([]);
-  const [targetMarker, setTargetMarker] = useState(undefined);
+const MapPage = ({ navigation }: AppStackProps<'Map'>) => {
+  const P0 = { latitude: 33.45061368551521, longitude: 126.56895152804822 }
+  const [markers, setMarkers] = useState<any[]>([])
+  const [targetMarker, setTargetMarker] = useState(undefined)
 
   useEffect(() => {
-    setMarkers(dummyMarker.data.bike);
-  }, []);
+    setMarkers(dummyMarker.data.bike)
+  }, [])
 
-  const goBack = () => {
-    navigation.goBack();
-  };
+  const goBack = () => navigation.goBack()
 
   return (
     <View style={tw`relative w-full h-full`}>
@@ -36,14 +34,11 @@ const MapPage = ({navigation}: AppStackProps<'Map'>) => {
         </Pressable>
       </View>
 
-      <NaverMapView
-        style={tw`flex-1`}
-        showsMyLocationButton={true}
-        center={{...P0, zoom: 16}}>
+      <NaverMapView style={tw`flex-1`} showsMyLocationButton={true} center={{ ...P0, zoom: 16 }}>
         {markers.map(marker => (
           <Marker
             key={marker.bike_id}
-            coordinate={{longitude: marker.gps[0], latitude: marker.gps[1]}}
+            coordinate={{ longitude: marker.gps[0], latitude: marker.gps[1] }}
             width={60}
             height={60}
             // image={require('../assets/reported.png')}
@@ -54,11 +49,8 @@ const MapPage = ({navigation}: AppStackProps<'Map'>) => {
                 // source={require('../assets/stand_by.png')}
                 source={iotStatusStyle[marker?.status]?.marker}
               />
-              <View
-                style={tw`absolute top-2 left-2 w-6 border p-[1px] rounded-md`}>
-                <View
-                  style={tw`bg-black h-1 w-[${marker.battery}%]  rounded-md`}
-                />
+              <View style={tw`absolute top-2 left-2 w-6 border p-[1px] rounded-md`}>
+                <View style={tw`bg-black h-1 w-[${marker.battery}%]  rounded-md`} />
               </View>
             </View>
           </Marker>
@@ -66,7 +58,7 @@ const MapPage = ({navigation}: AppStackProps<'Map'>) => {
       </NaverMapView>
       <IotController setTargetMarker={setTargetMarker} marker={targetMarker} />
     </View>
-  );
-};
+  )
+}
 
-export default MapPage;
+export default MapPage
