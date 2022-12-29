f5e9169861d1c800ebfd987b43967c15473ab263
MinHwan <gozld5153@naver.com>
2022-12-29 18:15:44 +0900

    Delet filter

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -31,7 +31,7 @@ const MapPage = ({ navigation }: AppStackProps<'Map'>) => {
   const handleCameraChanged = useCallback(async (e: OnCameraChangedEvent) => {
     try {
       const res = await axios.post<{ result: Iot[] }>('/iot/map', { region: e.coveringRegion })
-      setMarkers(res.data.result.filter(i => i.status !== 'in-use'))
+      setMarkers(res.data.result)
     } catch (error) {
       console.log(error)
     }
